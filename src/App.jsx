a2d9dcc44ec964c0a7b701623eacda0f51374ab1
ashirwadexe <ashirwadbappy@gmail.com>
2024-11-19 18:16:38 +0530

    learning

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+//LIST AND KEYS  -------------------------->
+
+// function App() {
+
+//   const items = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+
+//   return (
+//     <>
+//       {
+//         items.map((item, index) => (
+//           <h1 key={index}>{item}</h1>
+//         ))
+//       }
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
